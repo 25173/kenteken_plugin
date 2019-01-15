c505fe966f9d260af2e31872c531e8b165767b72
Timothy <31570626+25173@users.noreply.github.com>
2019-01-15 09:48:11 +0100

    Add files via upload

---
+++ js/scriptKenteken.js
@@ -0,0 +1,11 @@
+// const submit = document.getElementById('submit_kenteken');
+// const kenteken = document.getElementById('kenteken');
+//
+//
+// console.log('hello world');
+//
+// submit.addEventListener('click',searchKenteken(kenteken.value));
+//
+// function searchKenteken(kenteken1) {
+//     console.log(kenteken1)
+// }
