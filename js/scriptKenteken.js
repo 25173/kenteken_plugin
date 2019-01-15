166e9557cb624dabf055553aa279b001dfc5f441
Timothy <31570626+25173@users.noreply.github.com>
2019-01-15 15:59:43 +0100

    Add files via upload

--- js/scriptKenteken.js
+++ js/scriptKenteken.js
@@ -1,11 +1,12 @@
-// const submit = document.getElementById('submit_kenteken');
-// const kenteken = document.getElementById('kenteken');
-//
-//
-// console.log('hello world');
-//
-// submit.addEventListener('click',searchKenteken(kenteken.value));
-//
-// function searchKenteken(kenteken1) {
-//     console.log(kenteken1)
-// }
+const submit = document.getElementById('submit_kenteken');
+const kenteken = document.getElementById('kenteken');
+
+
+submit.addEventListener('click',searchKenteken);
+function searchKenteken() {
+    let license = kenteken.value.replace('-' ,  '');
+    window.location = 'https://autorapport.finnik.nl/kenteken/' + license.toLowerCase();
+    // console.log('https://autorapport.finnik.nl/kenteken/' + license.toLowerCase())
+}
+
+
--- namaak_kenteken.php
+++ namaak_kenteken.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name:  namaak kenteken check
 Plugin URI:   http://www.stackey.nl/bewijzenmap/periode2.1/cms/wordpress/
-Description:  een invul frame om je kenteken in te voeren. er wordt automatisch een database aangemaakt en voor display gebruik je [licensePlateCheck].
-Version:      1.0.0
+Description:  een invul frame om je kenteken in te voeren. het wordt doorverwezen naar een ander website die meer eigenschapen verteld over de auto. de shortcode is [licensePlateCheck].
+Version:      2.0.0
 Author:       Timothy Falorni
 Author URI:   falorni.nl
 License:      GPL2
@@ -20,7 +20,7 @@ function wpse_load_plugin_css() {
 	$plugin_url = plugin_dir_url( __FILE__ );
 
 	wp_enqueue_style( 'kenteken', $plugin_url . 'css/kenteken.css' );
-	wp_enqueue_script( 'kenteken', plugin_dir_url( __FILE__ ) . 'js/scriptKenteken.js', array( 'jquery' ), null, true );
+	wp_enqueue_script( 'kenteken', $plugin_url . 'js/scriptKenteken.js', array( 'jquery' ), null, true );
 
 }
 add_action( 'wp_enqueue_scripts', 'wpse_load_plugin_css' );
@@ -28,55 +28,16 @@ add_action( 'wp_enqueue_scripts', 'wpse_load_plugin_css' );
 
 function kentekenCheckForm() {
 	// see if we can find a license plate
-	if ( isset( $_POST["kenteken"] ) ) {
-		global $wpdb;
-
-		$kenteken = $_POST["kenteken"];
-		// remove html char
-		$kenteken = htmlspecialchars($kenteken);
-		// remove the special charater like '-'
-		$kenteken = RemoveSpecialChapr($kenteken);
-		// set all to low chase
-		$kenteken = strtolower($kenteken);
-
-		echo "<table class='licensePlateResult'>";
-
-		$result = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}license_Plate WHERE licensePlate  LIKE '$kenteken'");
-		if ($result == NULL){
-			echo 'No result for <br> license plate:'  . $kenteken . '<br>';
-//			echo $wpdb->last_error;
-		}else {
-			foreach ( $result as $row ) {
- 				if ($row->pay == 1){
-					$name = $row->name;
-					$lname = $row->Lname;
-					$km = $row->km;
-				}else{
-					$name = '<a href="#">€3,99</a>';
-					$lname = '<a href="#">€3,99</a>';
-					$km = '<a href="#">€1,99</a>';
-				}
-				echo '<tr><th>owner</th> <td> ' . $name . '</td></tr>';
-				echo '<tr><th>owner last name</th><td>' . $lname . '</td></tr>';
-				echo '<tr><th>brand</th><td> ' . $row->brand . '</td></tr>';
-				echo '<tr><th>license plate</th><td>' . $row->licensePlateReal . '</td></tr>';
-				echo '<tr><th>km</th><td>'. $km.'</td></tr>';
-			}
-		}
-	echo    "               
-			</table>";
-
-	} else {
 		echo "<div class='gratisKentekenForm'>
 				<div class='gratisKentekenTitel'>
 					<h2>Gratis kenteken check</h2>
 				</div>
 				<div class='grid'>
-			 	<form class='kentekenForm' action='" . $_SERVER['REQUEST_URI'] . "' method='post'>
+			 	<div class='kentekenForm'>
 			 		<label for='kenteken'> vul hier je kenteken in:</label>
-					 <input type='text' id='kenteken' name='kenteken' required placeholder='43-rh-jv'><br>
-			 		<input type='submit' id='submit_kenteken' value='Bekijk gratis rapport' name='submit_kenteken' >
-			 	</form>
+					 <input type='text' id='kenteken' name='kenteken' required placeholder='XX-123-X'><br>
+			 	<button  id='submit_kenteken' >Bekijk gratis rapport</button>
+			 	</div>				
 			 	<div class='kentekenchecks'>
 					 <ul>
 					 	<li>exacte waarde</li>
@@ -89,43 +50,8 @@ function kentekenCheckForm() {
 			 </div>
 			 </div>";
 
-	}
 }
 
 add_shortcode('licensePlateCheck', 'kentekenCheckForm');
 
-// making a database table for the license plate of all cars
-function my_plugin_create_db() {
-	global $wpdb;
-	$table_name = $wpdb->prefix . 'license_Plate';
 
-	// check if it exist or not
-	if ($wpdb->get_var('SHOW TABLES LIKE ' . $table_name) != $table_name) {
-//		create table for the license Plate
-		$sql = 'CREATE TABLE ' . $table_name . '(
-		id INTEGER(10) UNSIGNED AUTO_INCREMENT,
-		name TEXT(255),
-		Lname TEXT(255),
-		licensePlate VARCHAR(20),
-		licensePlateReal VARCHAR(225),
-		brand VARCHAR(225),
-		km INT(20),
-		pay INT DEFAULT 0 NOT NULL, 
-		PRIMARY KEY  (id) )';
-
-		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-		dbDelta($sql);
-
-		add_option('kenteken_databaseversion','1.0');
-	}
-}
-
-register_activation_hook( __FILE__, 'my_plugin_create_db' );
-
-
-// a function that remove all the special character
-function RemoveSpecialChapr($value){
-	$title = str_replace( array('-','/','_','.'),  '', $value);
-
-	return $title;
-}
